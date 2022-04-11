21788e994fc797bd99540c517724afb18b8432d6
Mark Ryan <markrryan99@gmail.com>
2022-04-11 16:36:02 -0700

    done like dinner

--- queries.js
+++ queries.js
@@ -4,6 +4,8 @@ const getAllBusinesses = async () => {
   try {
     //  Should find all businesses
     //  return the result of your query
+    const allTheBusiness = await Business.findAll()
+    return allTheBusiness
   } catch (error) {
     console.log(error)
   }
@@ -13,6 +15,8 @@ const getBusinessAddress = async () => {
   try {
     // Should find all businesses and their associated address
     //  return the result of your query
+    const insideTheBusinessesAddress = await Business.findAll({include: Address})
+    return insideTheBusinessesAddress
   } catch (error) {
     console.log(error)
   }
@@ -22,6 +26,8 @@ const getBusinessEmployees = async () => {
   try {
     // Should find all businesses with and associated employees
     //  return the result of your query
+    const whereTheEmployeesWork = await Business.findAll({include: Employee})
+    return whereTheEmployeesWork
   } catch (error) {
     console.log(error)
   }
@@ -32,6 +38,8 @@ const getBusinessAddressAndEmployee = async () => {
     //  Find all businesses and include the address and empoyees
     // The address should come before the employee
     //  return the result of your query
+    const letsGetItAll = await Business.findAll({include: [Address, Employee]})
+    return letsGetItAll
   } catch (error) {
     console.log(error)
   }
