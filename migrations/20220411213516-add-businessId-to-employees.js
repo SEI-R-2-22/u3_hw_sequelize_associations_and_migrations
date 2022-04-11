b5ffacb431d8739390b5de9c5f8801eb2f239288
Piero Caceres <pierocaceres92@yahoo.com>
2022-04-11 17:48:15 -0400

    Reached MVP

---
+++ migrations/20220411213516-add-businessId-to-employees.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+     return queryInterface.addColumn('employees', 'businessId', {
+      type: Sequelize.INTEGER,
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    return queryInterface.removeColumn('employees', 'businessId')
+  }
+};
--- queries.js
+++ queries.js
@@ -3,7 +3,9 @@ const { Business, Address, Employee } = require('./models')
 const getAllBusinesses = async () => {
   try {
     //  Should find all businesses
+    const businesses = await Business.findAll()
     //  return the result of your query
+    return businesses
   } catch (error) {
     console.log(error)
   }
@@ -12,7 +14,9 @@ const getAllBusinesses = async () => {
 const getBusinessAddress = async () => {
   try {
     // Should find all businesses and their associated address
+    const businesses = await Business.findAll({include: Address})
     //  return the result of your query
+    return businesses
   } catch (error) {
     console.log(error)
   }
@@ -21,7 +25,9 @@ const getBusinessAddress = async () => {
 const getBusinessEmployees = async () => {
   try {
     // Should find all businesses with and associated employees
+    const businesses = await Business.findAll({include: Employee})
     //  return the result of your query
+    return businesses
   } catch (error) {
     console.log(error)
   }
@@ -31,7 +37,9 @@ const getBusinessAddressAndEmployee = async () => {
   try {
     //  Find all businesses and include the address and empoyees
     // The address should come before the employee
+    const businesses = await Business.findAll({include: [Address, Employee]})
     //  return the result of your query
+    return businesses
   } catch (error) {
     console.log(error)
   }
