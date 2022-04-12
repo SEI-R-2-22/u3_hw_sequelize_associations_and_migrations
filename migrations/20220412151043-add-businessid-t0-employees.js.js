4f453135d106a372b134672d9da77300b6bac40d
Eloy Sauceda <eloysauceda4@outlook.com>
2022-04-12 10:31:13 -0500

    submission

---
+++ migrations/20220412151043-add-businessid-t0-employees.js.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return 
+    queryInterface.addColumn('employees', 
+    'businessId', {
+      type: Sequelize.INTEGER
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+  return 
+  queryInterface.removeColumn('employees', 'businessId')
+  }
+};
