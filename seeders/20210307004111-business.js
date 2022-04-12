b872c561fce76a3a998b1c5fc3464181d2569563
Eric Lantz <ericmlantz@gmail.com>
2022-04-11 22:35:27 -0400

    Homework fix

--- seeders/20210307004111-business.js
+++ seeders/20210307004111-business.js
@@ -11,6 +11,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('businesses')
+    await queryInterface.bulkRemove('businesses')
   }
 }
