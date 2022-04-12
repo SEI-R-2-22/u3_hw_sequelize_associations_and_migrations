3c4ec0989ae6292243989e14250292a10e1244ab
Mark Harmon <neothej1sec@gmail.com>
2022-04-11 20:29:06 -0500

    finished querys and npm test not working

--- queries.js
+++ queries.js
@@ -2,6 +2,8 @@ const { Business, Address, Employee } = require('./models')
 
 const getAllBusinesses = async () => {
   try {
+    const business = await Business.findAll()
+    return business
     //  Should find all businesses
     //  return the result of your query
   } catch (error) {
@@ -11,6 +13,8 @@ const getAllBusinesses = async () => {
 
 const getBusinessAddress = async () => {
   try {
+    const busWithAdd = await Business.findAll({include: Address})
+    return busWithAdd
     // Should find all businesses and their associated address
     //  return the result of your query
   } catch (error) {
@@ -20,6 +24,8 @@ const getBusinessAddress = async () => {
 
 const getBusinessEmployees = async () => {
   try {
+    const busWithEmp = await Business.findAll({include: Employee})
+    return busWithEmp
     // Should find all businesses with and associated employees
     //  return the result of your query
   } catch (error) {
@@ -29,6 +35,11 @@ const getBusinessEmployees = async () => {
 
 const getBusinessAddressAndEmployee = async () => {
   try {
+    const all = await Business.findAll(
+      {include: Address },
+      {include: Employee}
+    )
+    return all
     //  Find all businesses and include the address and empoyees
     // The address should come before the employee
     //  return the result of your query
