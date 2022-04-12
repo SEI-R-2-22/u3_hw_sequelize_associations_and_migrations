3b4a9a904f45b010dba730c543f948699df8a6c6
Michelle Flores <lmfg2456@gmail.com>
2022-04-11 22:46:00 -0700

    errorrrrs

---
+++ migrations/20220412051215-add-businessId-to-employees.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+   return queryInterface.addColumn('employees', 'businessId',{
+     type:Sequelize.INTEGER,
+     onDelete: 'CASCADE',
+     references: {
+       model: 'businesses',
+       key:'id'
+     }
+   })
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.removeColumn('employees', 'businessId')
+  }
+};
