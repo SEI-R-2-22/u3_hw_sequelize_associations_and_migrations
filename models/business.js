8cf89bf5fb59bb75abc70807e79c02afdd08f650
John A. Jacobs <john.a.jacobs1973@gmail.com>
2022-04-12 08:12:50 -0400

    Revert "Group 3 - Chinwendu Ukoha"

--- models/business.js
+++ models/business.js
@@ -8,8 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Business.hasMany(models.Employee, { foreignKey: 'businessId' })
-      Business.hasOne(models.Address, { foreignKey: 'businessId' })
+      // define association here
     }
   }
   Business.init(
--- queries.js
+++ queries.js
@@ -2,9 +2,8 @@ const { Business, Address, Employee } = require('./models')
 
 const getAllBusinesses = async () => {
   try {
-    const businesses = await Business.findAll()
-    // stringify(businesses)
-    return businesses
+    //  Should find all businesses
+    //  return the result of your query
   } catch (error) {
     console.log(error)
   }
@@ -12,8 +11,8 @@ const getAllBusinesses = async () => {
 
 const getBusinessAddress = async () => {
   try {
-    const businessAddress = await Business.findAll({ include: Address })
-    return businessAddress
+    // Should find all businesses and their associated address
+    //  return the result of your query
   } catch (error) {
     console.log(error)
   }
@@ -21,8 +20,8 @@ const getBusinessAddress = async () => {
 
 const getBusinessEmployees = async () => {
   try {
-    const businessEmployees = await Business.findAll({ include: Employee })
-    return businessEmployees
+    // Should find all businesses with and associated employees
+    //  return the result of your query
   } catch (error) {
     console.log(error)
   }
@@ -30,8 +29,9 @@ const getBusinessEmployees = async () => {
 
 const getBusinessAddressAndEmployee = async () => {
   try {
-    const businesses = await Business.findAll({ include: [Address, Employee] })
-    return businesses
+    //  Find all businesses and include the address and empoyees
+    // The address should come before the employee
+    //  return the result of your query
   } catch (error) {
     console.log(error)
   }
