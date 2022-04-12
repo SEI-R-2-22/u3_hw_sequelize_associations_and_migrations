4a4c0b9d307130b2e6e721710f761b896e2edbf1
Chinwendu <chinwenduukoha@gmail.com>
2022-04-11 20:27:01 -0400

    complete

--- models/employee.js
+++ models/employee.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Employee.belongsTo(models.Business, { foreignKey: 'businessId' })
     }
   }
   Employee.init(
