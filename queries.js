4e84200a21967ecc1204b9f9adfa1586a87e5d1c
Michelle Yang <myang292@gmail.com>
2022-04-11 21:40:53 -0700

    Group 2 - Michelle Yang

--- queries.js
+++ queries.js
@@ -2,8 +2,8 @@ const { Business, Address, Employee } = require('./models')
 
 const getAllBusinesses = async () => {
   try {
-    //  Should find all businesses
-    //  return the result of your query
+    const business = await Business.findAll()
+    return business
   } catch (error) {
     console.log(error)
   }
@@ -11,8 +11,10 @@ const getAllBusinesses = async () => {
 
 const getBusinessAddress = async () => {
   try {
-    // Should find all businesses and their associated address
-    //  return the result of your query
+    const business = await Business.findAll({
+      inlude: { mode: Address }
+    })
+    return business
   } catch (error) {
     console.log(error)
   }
@@ -20,8 +22,10 @@ const getBusinessAddress = async () => {
 
 const getBusinessEmployees = async () => {
   try {
-    // Should find all businesses with and associated employees
-    //  return the result of your query
+    const business = await Business.findAll({
+      inlude: { mode: Employee }
+    })
+    return business
   } catch (error) {
     console.log(error)
   }
@@ -29,9 +33,10 @@ const getBusinessEmployees = async () => {
 
 const getBusinessAddressAndEmployee = async () => {
   try {
-    //  Find all businesses and include the address and empoyees
-    // The address should come before the employee
-    //  return the result of your query
+    const business = await Business.findAll({
+      inlude: { mode: [Employee, Address] }
+    })
+    return business
   } catch (error) {
     console.log(error)
   }
