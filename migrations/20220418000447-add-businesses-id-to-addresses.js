3fc4f2b19da11139dd35606f0c7c9ac61327df8a
Ifeoluwa Adebisi <adebisiife@gmail.com>
2022-04-18 07:46:44 -0400

    save

---
+++ migrations/20220418000447-add-businesses-id-to-addresses.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    return queryInterface.addColumn('addresses','businessId',{
+      type:Sequelize.INTEGER
+      }
+    );
+  },
+
+  async down (queryInterface, Sequelize) {
+    return queryInterface.removeColumn('addresses', 'businessId');
+  }
+};
